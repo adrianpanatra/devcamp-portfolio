2811403102efee424ab75b75753f4af1602745ca
Adrian Julianto <juliantoadrian@gmail.com>
2019-08-02 09:17:21 +0700

    portfolio action and cleaning up somethings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_091920) do
+ActiveRecord::Schema.define(version: 2019_08_01_095537) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
