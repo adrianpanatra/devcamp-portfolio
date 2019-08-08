c0b2f63441ad3422b3d768470a92ef8174dff34f
Adrian Julianto <juliantoadrian@gmail.com>
2019-08-08 11:00:49 +0700

    refactoring strong params

--- config/application.rb
+++ config/application.rb
@@ -8,13 +8,7 @@ Bundler.require(*Rails.groups)
 
 module DevcampPortfolio
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.generators do |g|
-      g.orm             :active_record
-      g.template_engine :erb
-      g.test_framework  :test_unit, fixture:  false
-      g.stylesheets     :false
-      g.javascripts     :false
+
     end
   end
 end
